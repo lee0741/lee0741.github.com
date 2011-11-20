0f6afaa465d2f4d0a1a4de4568baa9c7ae90a5d8
Steven Lee <lee0741@gmail.com>
2011-11-20 16:30:23 +0800

    add a bash script

---
+++ push_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then 
+	commit_message="$1"
+else
+	commit_message="Updated site `date`."
+fi
+
+git add .
+git commit -a -m "$commit_message"
+git push origin master
